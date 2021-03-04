0dca7726f8af342db8da47cd890bf9206606b058
jennmchenry1 <jennmchenry1@gmail.com>
2021-03-04 14:53:19 -0500

    Ordered imports according to PEP8 2

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -1,4 +1,5 @@
 import argparse
+
 import xarray as xr
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
@@ -62,7 +63,7 @@ def main(inargs):
 
 
 if __name__ == '__main__':
-    description='Plot the precipitation climatology.'
+    description='Plot the precipitation climatology for a given season.'
     parser = argparse.ArgumentParser(description=description)
     
     parser.add_argument("pr_file", type=str, help="Precipitation data file")
